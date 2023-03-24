214b3ff235ccb09fa18e5c072d4be94c12d7cc69
root <root@ip-172-31-27-255.ap-southeast-1.compute.internal>
2023-03-24 10:02:45 +0000

    boolean usage

--- main.tf
+++ main.tf
@@ -1,15 +1,20 @@
-
-resource "aws_iam_user" "key2" {
-name = var.c
-}
-resource "aws_iam_group" "key3" {
-name = var.d
+provider "aws" {
+region= "ap-south-1"
+access_key="AKIASO54IO3G53OEV7BV"
+secret_key="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"
 }
 
-resource "aws_instance" "key1" {
-ami= var.ami_id
-instance_type= var.b
-tags= {
-Name= "pavan"
+resource "aws_instance" "one" {
+ami="ami-0376ec8eacdf70aae"
+instance_type= "t2.micro"
+associate_public_ip_address=var.ab
+tags={
+Name= "venk1"
 }
 }
+
+variable "ab" {
+type=bool
+default=true
+}
+
