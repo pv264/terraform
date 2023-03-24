6cc5c62757673c100f9b145f9cb383c5713774cc
root <root@ip-172-31-27-255.ap-southeast-1.compute.internal>
2023-03-24 10:24:52 +0000

    main

--- main.tf
+++ main.tf
@@ -1,20 +1,18 @@
-provider "aws" {
-region= "ap-south-1"
-access_key="AKIASO54IO3G53OEV7BV"
-secret_key="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"
-}
+
 
 resource "aws_instance" "one" {
-ami="ami-0376ec8eacdf70aae"
-instance_type= "t2.micro"
-associate_public_ip_address=var.ab
+ami=var.ami_id
+instance_type=var.b
 tags={
-Name= "venk1"
+Name= "venkat"
 }
 }
 
-variable "ab" {
-type=bool
-default=true
+
+resource "aws_iam_user" "key"{
+name= var.c
 }
 
+resource "aws_iam_group" "key1"{
+name=var.d
+}
